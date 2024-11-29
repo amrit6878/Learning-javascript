64e4c12b929bcce0c086a163ade0b62925a6ce26
amrit6878 <asinghbaraura.123@gmail.com>
2024-11-29 20:02:47 +0000

    change in datatype

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -36,3 +36,17 @@ console.log(typeof(myFunction));
 console.log(typeof(anotherId));
 
 
+// NOTE:-
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
